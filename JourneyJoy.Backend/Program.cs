ea93eea95a9ef1ab11eb0636eb8826a7437b5632
nobster14 <01161350@pw.edu.pl>
2023-11-04 16:37:51 +0100

    test deployu

--- JourneyJoy.Backend/Program.cs
+++ JourneyJoy.Backend/Program.cs
@@ -9,12 +9,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
