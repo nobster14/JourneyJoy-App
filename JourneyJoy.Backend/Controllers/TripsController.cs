8797e1629041078bcb3af00ed77e70b90d5a3013
nobster14 <01161350@pw.edu.pl>
2023-11-21 17:22:56 +0100

    Dodanie pustego kontrolera wycieczek

---
+++ JourneyJoy.Backend/Controllers/TripsController.cs
@@ -0,0 +1,30 @@
+﻿using JourneyJoy.Backend.Options;
+using JourneyJoy.Contracts;
+using JourneyJoy.ExternalAPI;
+using JourneyJoy.Utils.Security.HashAlgorithms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace JourneyJoy.Backend.Controllers
+{
+    [Route("trips")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        #region Fields
+
+        private AppOptions settings;
+        private IRepositoryWrapper repositoryWrapper;
+        private IExternalApiService externalApiService;
+        #endregion
+
+        #region Constructors
+        public TripsController(IOptions<AppOptions> settings, IRepositoryWrapper repositoryWrapper, IExternalApiService externalApiService)
+        {
+            this.settings = settings.Value;
+            this.repositoryWrapper = repositoryWrapper;
+            this.externalApiService = externalApiService;
+        }
+        #endregion
+    }
+}
