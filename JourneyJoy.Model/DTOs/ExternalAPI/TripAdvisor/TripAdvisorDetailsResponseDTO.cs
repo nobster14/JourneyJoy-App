803b70f10e90307de0349e930f28f525e3822da6
nobster14 <115613105+nobster14@users.noreply.github.com>
2023-12-10 16:15:28 +0100

    wstepne pobieranie godzin otwarcia

---
+++ JourneyJoy.Model/DTOs/ExternalAPI/TripAdvisor/TripAdvisorDetailsResponseDTO.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyJoy.Model.DTOs.ExternalAPI.TripAdvisor
+{
+    public record TripAdvisorDetailsResponseDTO
+    {
+        [JsonProperty("location_id")]
+        public int LocationId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("web_url")]
+        public string WebUrl { get; set; }
+
+        [JsonProperty("address_obj")]
+        public TripAdvisorAddressDTO Address { get; set; }
+    }
+}
