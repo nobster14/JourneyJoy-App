a6c1f620738edd7890ec50f1b23251c20974e100
nobster14 <115613105+nobster14@users.noreply.github.com>
2024-01-11 20:04:10 +0100

    Dodanie AttractionsNotOnRoute w TripDTO

--- JourneyJoy.Model/DTOs/TripDTO.cs
+++ JourneyJoy.Model/DTOs/TripDTO.cs
@@ -16,12 +16,12 @@ namespace JourneyJoy.Model.DTOs
         public string? Description { get; set; }
         public string? Picture { get; set; }
         public List<AttractionDTO> Attractions { get; set; }
-
+        public List<Guid> AttractionsNotOnRoute { get; set; }
         public RouteDTO Route { get; set; } 
 
         public static TripDTO FromDatabaseTrip(Trip trip)
         {
-            return new TripDTO()
+            var retTrip =  new TripDTO()
             {
                 ID = trip.Id.ToString(),
                 Description = trip.Description,
@@ -31,6 +31,14 @@ namespace JourneyJoy.Model.DTOs
                 Picture = trip.Photo,
                 Route = trip.Route == null ? null : RouteDTO.FromDatabaseRoute(trip.Route),
             };
+
+            if (retTrip.Route != null && trip.Attractions != null)
+                retTrip.AttractionsNotOnRoute = retTrip.Attractions
+                    .Select(it => it.Id)
+                    .Where(it => retTrip.Route.StarePointAttractionId != it && retTrip.Route.AttractionsInOrder.Any(it2 => it2.Contains(it)))
+                    .ToList();
+
+            return retTrip;
         } 
     }
 }
